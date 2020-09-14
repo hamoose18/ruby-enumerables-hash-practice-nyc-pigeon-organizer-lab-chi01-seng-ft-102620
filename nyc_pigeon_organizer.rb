dab3fc9e0de5c538d893c85921db485b0ccb42be
arhamm ghazali <arhamm_ghazali93@hotmail.com>
2020-09-14 17:42:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
         if !final_array[name]
           final_array[name] = {}
         end
-        
+        binding.pry
         if !final_array[name][key]
           !final_array[name][key] = []
         end
